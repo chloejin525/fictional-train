435c1c6718e3d1eca80666ed29156c3ee5165bae
Ling Jin <chloejin525@gmail.com>
2017-07-26 09:33:21 -0400

    changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:new, keys: [:avatar])
-  	devise_parameter_sanitizer.permit(:edit, keys: [:avatar])
-  end
 end
